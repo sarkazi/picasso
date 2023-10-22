102a020f4b976e3a69abd8bce692843582bd1e68
Morozov Nikolay <nikemorozow@gmail.com>
2023-10-22 12:35:13 +0300

    add console

--- src/shared/lib/hooks/useInfiniteScroll.jsx
+++ src/shared/lib/hooks/useInfiniteScroll.jsx
@@ -10,6 +10,14 @@ export const useInfiniteScroll = ({ totalCount, posts, setCurrentPage }) => {
   }, [totalCount, posts]);
 
   const scrollHandler = (event) => {
+    console.log(
+      event.target.documentElement.scrollHeight,
+      event.target.documentElement.scrollTop,
+      window.innerHeight,
+      event.target.documentElement.scrollHeight -
+        (event.target.documentElement.scrollTop + window.innerHeight),
+    );
+
     if (
       event.target.documentElement.scrollHeight -
         (event.target.documentElement.scrollTop + window.innerHeight) ===
